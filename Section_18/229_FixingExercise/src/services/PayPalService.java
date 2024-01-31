d5419ca4fbe2c5397eae494b547951310f616c27
Desktop Marllon <marllonrsantos@gmail.com>
2024-01-30 22:42:32 -0300

    Feat: Class 230, Implemeting the classes

---
+++ Section_18/229_FixingExercise/src/services/PayPalService.java
@@ -0,0 +1,13 @@
+package services;
+
+public class PayPalService implements OnlinePaymentService{
+
+	public double paymentFee(double amount) {
+		return 0;
+		
+	}
+	
+	public double interest(double amount, int months) {
+		return 0;
+	}
+}
